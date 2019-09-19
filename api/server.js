e3593de01de078a51f61b31aa7aadb98b0214a89
Noor Al Darraji <077068@yahoo.com>
2019-09-19 10:25:20 -0500

    WIP: session cookie practice :hammer:

--- api/server.js
+++ api/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const session = require('express-session');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'lowkey',
+  secret: process.env.SESSION_SECRET || 'keep it warm please!',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
